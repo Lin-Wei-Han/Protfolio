ea9be0b43a82f6d141fcd7fefa425f2618f28d29
Lin-Wei-Han <judy97678@gmail.com>
2023-03-02 14:00:37 +0800

    @feat:add project select button

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -13,61 +13,90 @@ const projectDetail = [
         title: "六都房價趨勢與預測儀表板",
         info: "以內政部實價登陸公布隻交易紀錄，實現六都房價走勢儀表板，並且以隨機數森林創建預測模型，針對房價做預測。亦提供嚴謹的統計檢定結果，分析六都差異。",
         img: houseprice,
-        url: "https://taiwanhouseprice.herokuapp.com/"
+        url: "https://taiwanhouseprice.herokuapp.com/",
+        type: ["data", "web"]
     },
     {
         title: "Covid-19 近況數據儀表版",
         info: "透過R語言、Data Visualization與R shiny，將Covid-19對於產業衝擊的影響製作成數據儀表板。並即時產生簡易的描述性統計，方便觀察者知道數據變動的意義。",
         img: shinydashboard,
-        url: "https://shinycovid-19.herokuapp.com/"
+        url: "https://shinycovid-19.herokuapp.com/",
+        type: ["data", "web"]
     },
     {
         title: "即時物件偵測",
         info: "透過tensorflow官方所提供的深度學習框架，串接至React上，透過網站達到即時偵測的效果，並且能偵測多達80多件不同的物件。",
         img: reactobjectdetect,
-        url: "https://computervisiontemplate.herokuapp.com/"
+        url: "https://computervisiontemplate.herokuapp.com/",
+        type: ["data"]
     },
     {
         title: "全國交通事故類型 Dashboard",
         info: "利用 Power BI 串接全國交通事故統計資料，以視覺化圖表呈現各地區車禍事故的分布熱點，並進一步觀察主要發生交通事故的肇事主因。並公開部署至雲端，供民眾使用。",
         img: powerbi_trafic,
-        url: "https://reurl.cc/6Lnmv5"
+        url: "https://reurl.cc/6Lnmv5",
+        type: ["data", "web"]
     },
     {
         title: "全球新冠肺炎趨勢 Dashboard",
         info: "利用 Power BI 串接衛服部資料庫，抓取全球確診數及本國每日確診等即時資訊。協助使用者觀察疫情發展近況，並公開部署至雲端，供民眾使用。",
         img: power_bi_covid,
-        url: "https://reurl.cc/x1VmL5"
+        url: "https://reurl.cc/x1VmL5",
+        type: ["data"]
     },
     {
         title: "麥索迪斯企業形象網站",
         info: "協助企業打造形象網站，瞄準目標客群，使客戶能夠迅速了解企業的服務項目。不僅網站資訊清晰，並加入動態元素，創造更具現代化的形象網站。(目前暫停使用)",
         img: myosotis,
-        url: "https://myosotis.online/"
+        url: "https://myosotis.online/",
+        type: ["web"]
     },
     {
         title: "競賽活動網站",
         info: "針對年輕族群，設計風格、配色較多元且鮮豔，且喜歡加入動態效果，添加活潑的元素，不僅力求瀏覽體驗也加深了使用者的印象。",
         img: palcelantern,
-        url: "https://lin-wei-han.github.io/Palace-Lantern/index.html?fbclid=IwAR0AS0zu7vIVZvwMUOh4uC2oq2X_R12JEvKQ9cgu7hvydXkQOaVkDrnwoXE"
+        url: "https://lin-wei-han.github.io/Palace-Lantern/index.html?fbclid=IwAR0AS0zu7vIVZvwMUOh4uC2oq2X_R12JEvKQ9cgu7hvydXkQOaVkDrnwoXE",
+        type: ["web"]
     },
 ]
 
-const ProjectList = () => {
+const ProjectList = (props) => {
+    let tabContactContent = props.tabContactContent
+
+    React.useEffect(() => {
+        console.log(tabContactContent);
+    }, [tabContactContent])
+
     return (
         <div className={style()}>
             {projectDetail.map((item, index) => (
-                <div className='wrapper' key={index}>
-                    <div className="card front-face">
-                        <img src={item.img} alt='Project' />
-                    </div>
-                    <div className="card back-face">
-                        <div className="info">
-                            <div className="title"><a href={item.url}>{item.title}</a></div>
-                            <p>{item.info}</p>
+                <>
+                    {tabContactContent === "all" ?
+                        <div className='wrapper' key={index}>
+                            <div className="card front-face">
+                                <img src={item.img} alt='Project' />
+                            </div>
+                            <div className="card back-face">
+                                <div className="info">
+                                    <div className="title"><a href={item.url}>{item.title}</a></div>
+                                    <p>{item.info}</p>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                        : item.type.includes(tabContactContent) ?
+                            <div className='wrapper' key={index}>
+                                <div className="card front-face">
+                                    <img src={item.img} alt='Project' />
+                                </div>
+                                <div className="card back-face">
+                                    <div className="info">
+                                        <div className="title"><a href={item.url}>{item.title}</a></div>
+                                        <p>{item.info}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            : ""}
+                </>
             ))}
         </div>
     )
@@ -79,7 +108,15 @@ export const style = () => css`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     padding: 5rem 2rem;
-    animation: project 0.8s ease-out 0.5s forwards ;
+
+    @keyframes scaleUp {
+        from {
+            transform: scale(0);
+        }
+        to {
+            transform: scale(1);
+        }
+        }
 
     .wrapper{
         height: 350px;
@@ -89,6 +126,8 @@ export const style = () => css`
         position: relative;
         transform-style: preserve-3d;
         perspective: 1000px;
+        animation : scaleUp 0.3s forwards;
+        transition: all 0.5s ease;
         .card{
             position: absolute;
             height: 100%;
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { motion } from "framer-motion";
-import { css } from '@emotion/css/macro';
+import { css, cx } from '@emotion/css/macro';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import ProjectList from '../components/ProjectList';
 
+const tabContent = {
+    all: "all",
+    web: "web",
+    data: "data"
+};
+
 const Project = () => {
+
+    const [isBegin, setIsBegin] = React.useState(true);
+    const [tabContactContent, setTabContactContent] = React.useState(tabContent.all);
+
+    const handleChangeContent = (tabContactType) => setTabContactContent(tabContactType);
+
+    React.useEffect(() => {
+        setTimeout(() => setIsBegin(false), 2000)
+    }, [isBegin])
 
     const blackBox = {
         initial: {
@@ -63,7 +78,7 @@ const Project = () => {
             initial="initial"
             animate="animate"
             variants={content}>
-            <InitialTransition />
+            {isBegin ? <InitialTransition /> : ""}
             <Navbar />
             <motion.section className={style()} variants={title}>
                 <div className="title">
@@ -71,7 +86,17 @@ const Project = () => {
                     <h2>PROJECT</h2>
                 </div>
             </motion.section>
-            <motion.section variants={title}><ProjectList /></motion.section>
+            <motion.section className={buttonProject()} variants={title}>
+                <div className='contact-button-list'>
+                    <div className={cx("contact-button", tabContactContent === tabContent.all ? "active" : "")}
+                        onClick={() => handleChangeContent(tabContent.all)}>ALL</div>
+                    <div className={cx("contact-button", tabContactContent === tabContent.web ? "active" : "")}
+                        onClick={() => handleChangeContent(tabContent.web)}>WEBSITE</div>
+                    <div className={cx("contact-button", tabContactContent === tabContent.data ? "active" : "")}
+                        onClick={() => handleChangeContent(tabContent.data)}>DATA ANALYSIS</div>
+                </div>
+            </motion.section>
+            <motion.section variants={title}><ProjectList tabContactContent={tabContactContent} /></motion.section>
             <Footer />
         </motion.div>
     )
@@ -125,5 +150,60 @@ export const style = () => css`
             }
         }
     
+}
+`
+
+export const buttonProject = () => css`
+        display: block;
+        margin: 0 auto;
+        width: 30%;
+        .contact-button-list{
+            display: flex;
+            justify-content: space-evenly;
+            margin: 3rem 0;
+            .contact-button{
+                color: crimson;
+                background: #FFFFFF;
+                border: 3px solid #DC143C;
+                border-radius: 25px;
+                padding:5px 8px ;
+                cursor: pointer;
+                &:hover,&.active{
+                    color: white;
+                    background: crimson;
+                    transition: 0.5s ease ; 
+                }
+            }
+        }
+
+@media screen and (max-width:1000px) { 
+    width: 45%;
+}
+
+@media screen and (max-width:680px) { 
+    width: 60%;
+}
+
+@media screen and (max-width:540px) {
+    width: 80%;
+}
+
+@media screen and (max-width:380px) {
+    width: 90%;
+    .contact-button-list{
+            .contact-button{
+                font-size: 12px;
+                border: 2px solid #DC143C;
+            }
+        }
+}
+
+@media screen and (max-width:290px) {
+    width: 97%;
+    .contact-button-list{
+            .contact-button{
+                font-size: 10px;
+            }
+        }
 }
 `
